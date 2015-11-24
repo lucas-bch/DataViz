f432b0f68a90eef4018161a49d7703181a1e50d3
Loisel Nathan <n.ninecc@gmail.com>
2015-11-24 20:45:04 +0100

    debut gestion des datas

--- dataviz/client/App.jsx
+++ dataviz/client/App.jsx
@@ -1,24 +1,11 @@
 App = React.createClass({
-  getWeathers: function() {
-    return [
-      { _id: 1, temperature: "23" },
-      { _id: 2, temperature: "24" },
-      { _id: 3, temperature: "25" }
-    ];
-  },
-  renderWeathers: function() {
-    return this.getWeathers().map((weather) => {
-      return <Weather key={weather._id} weather={weather} />;
-    });
-
-  },
   render: function() {
     return (
         <div className="weather-cover">
             <Header />
             <div className="weather-wrapper">
                 <div className="container">
-                    { this.renderWeathers() }
+                    <Weather key="1" />;
                 </div>
             </div>
             <div className="puncline-wrapper">
