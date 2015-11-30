b59b4121d3490640c1f7e2aa9703799612ae3fdd
Jean <tiner@hotmail.fr>
2015-12-01 00:01:51 +0100

    debut de la classe weathergraph

--- dataviz/client/Weather.jsx
+++ dataviz/client/Weather.jsx
@@ -1,4 +1,27 @@
+var data = {
+    // A labels array that can contain any sort of values
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+    // Our series array that contains series objects or in this case series data arrays
+    series: [
+        [5, 2, 4, 2, 0,2,1,3,4  ]
+    ]
+};
 
+var data2 = {
+    // A labels array that can contain any sort of values
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+    // Our series array that contains series objects or in this case series data arrays
+    series: [
+        [3, 4, 4, 2,3,2,1,1,4 ]
+    ]
+};
+
+// As options we currently only set a static size of 300x200 px
+var options = {
+    height: '250px',
+    low: 0,
+   Area: true
+};
 
 
 Weather = React.createClass({
@@ -9,11 +32,21 @@ Weather = React.createClass({
 
         return {
             temperature: 24,
-            name:  "Toulouse"
+            name:  "Toulouse",
+            weather : {}
         };
     },
-    componentDidMount: function() {
+
+    changeScale: function(e){
+        e.preventDefault();
+        console.log("lama")
+        this.state.weather.updateData(data2);
     },
+
+    componentDidMount : function(){
+        this.state.weather = new WeatherGraph(".ct-chart",options,data);
+    },
+
     render: function() {
         return (
             <div className="row weather-body">
@@ -30,7 +63,7 @@ Weather = React.createClass({
                     </div>
                 </div>
 
-                <div className="col m12 l12 cyan weather-content">
+                <div className="col m12 l12 weather-content">
                     <div className="container ">
 
                         <div className="card white darken-1">
@@ -38,7 +71,7 @@ Weather = React.createClass({
                                 <div className="row">
                                     <div className="col m2 l2 weather-options">
 
-                                        <a className="weather-option waves-effect waves-light btn-large">Hourly</a>
+                                        <a className="weather-option waves-effect waves-light btn-large" onClick={ this.changeScale }>Hourly</a>
 
                                         <a className="weather-option waves-effect waves-light btn-large">Daily</a>
 
@@ -65,4 +98,28 @@ Weather = React.createClass({
         );
     }
 });
+
+
+// My Chart
+var WeatherGraph = (function(){
+
+
+    function WeatherGraph (selector, options, data){
+        this.selector = selector;
+        this.data = data ;
+        this.options = options;
+        this.chart = new Chartist.Line(this.selector, data, options);
+    }
+
+    WeatherGraph.prototype.updateData = function(data){
+        var self = this;
+        if(undefined !== data){
+            self.data = data;
+            self.chart.update(self.data);
+        }
+        return this;
+    }
+
+    return WeatherGraph;
+})();
 
--- dataviz/client/body.jsx
+++ dataviz/client/body.jsx
@@ -1,22 +1,4 @@
 
-
-var data = {
-    // A labels array that can contain any sort of values
-    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-    // Our series array that contains series objects or in this case series data arrays
-    series: [
-        [5, 2, 4, 2, 0,2,1,3,4  ]
-    ]
-};
-
-// As options we currently only set a static size of 300x200 px
-var options = {
-    height: '250px',
-    low: 0,
-    showArea: true
-};
-
-
 //*******************************
 //              MAIN
 //*******************************
@@ -27,7 +9,6 @@ if (Meteor.isClient) {
   });
 
   $(document).ready(function(){
-    drawChart();
     // Initialize collapse button
     $(".button-collapse").sideNav({
       menuWidth: 300, // Default is 240
@@ -46,14 +27,7 @@ if (Meteor.isServer) {
   });
 }
 
-// Resize chart
 
 
 
 
-// Draw chart
-function drawChart() {
-
-    // In the global name space Chartist we call the Line function to initialize a line chart. As a first parameter we pass in a selector where we would like to get our chart created. Second parameter is the actual data object and as a third parameter we pass in our options
-    new Chartist.Line('.ct-chart', data, options);
-}
